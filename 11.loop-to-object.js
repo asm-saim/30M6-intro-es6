f68ddadf42b48bf7037ae8d69c4a162f875ec7e2
asm-saim <saim777m@gmail.com>
2026-06-12 23:58:54 +0600

    for in function in object

---
+++ 11.loop-to-object.js
@@ -0,0 +1,9 @@
+//for in function:
+const data = { name: "Yamin", age: 23, location: "Khulna", pen: 5 };
+for(const key in data)
+{
+    // console.log(key);
+    const value=data[key];
+    console.log(key, value);
+}
+
